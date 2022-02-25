f74c8262042bdf68fca40dfb1f1225cd16e16f9c
sasipreetha <30656181+sasipreetha@users.noreply.github.com>
2022-02-25 20:10:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Welcome to console");
+var a = "Sasi";
+var b = "Preetha";
+var c = a.concat(b);
+console.log("First Name",a);
+console.log("Second Name",b);
+console.log("Full Name",c);
